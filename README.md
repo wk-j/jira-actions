6c77a54a8c62a4ac8af4caf5e6cd148d0a9d5f56
wk-j <somnuk.wk@gmail.com>
2019-10-21 15:53:22 +0700

    Todo

---
+++ README.md
@@ -0,0 +1,6 @@
+## Jira Actions
+
+[![Actions](https://github.com/wk-j/jira-actions/workflows/jira-todo/badge.svg)](https://github.com/wk-j/jira-actions/actions)
+
+- TODO: Update README.md
+- TODO: Add source
---
+++ src/Todo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// TODO: Update README.md
+// TODO: Add source
+
+namespace Todo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
